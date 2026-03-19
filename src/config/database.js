5d9b2e48ab2c6defe0a65c2a9823b83c2451a2ba
Nguyễn Minh Trí - 3368 <minhtri1072004@gmail.com>
2026-03-19 23:02:43 +0700

    kết nối aiven để sử dụng db trên cloud

--- src/config/database.js
+++ src/config/database.js
@@ -4,28 +4,21 @@ require('dotenv').config();
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false 
+  }
 });
-// const connection = mysql.createPool({
-//     host:process.env.DB_HOST,
-//     user:process.env.DB_USER,
-//     password:process.env.DB_PASSWORD,
-//     database:process.env.DB_NAME,
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit:0
-//   })
 
-
-// Kiểm tra kết nối
 connection.connect((err) => {
     if (err) {
-      console.error('❌ Kết nối thất bại:', err.stack);
+      console.error(' Kết nối thất bại:', err.stack);
       return;
     }
-    console.log('✅ Kết nối thành công đến MySQL với ID:', connection.threadId);
+    console.log(' Kết nối thành công đến MySQL với ID:', connection.threadId);
   });
 module.exports = connection;
 
